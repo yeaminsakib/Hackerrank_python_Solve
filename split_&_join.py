a872b18eb98d6171fbc024e956ef6901c38788b6
yeaminsakib <yeaminsakib@gmail.com>
2025-02-13 19:43:35 +0600

    split & join 13 feb 25

---
+++ split_&_join.py
@@ -0,0 +1,11 @@
+
+def split_and_join(line):
+    # write your code here
+    x= line.split(" ")
+    y='-'.join(x)
+    return y
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
